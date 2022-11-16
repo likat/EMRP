884d9cc7e5f94556d305379e9116b9923336aa4e
Katherine Li <likat@Katherines-MacBook-Pro-3.local>
2022-11-16 15:33:30 -0500

    refined code for WFPBB and multinomial draws

--- simulation_final/update_wfpbbmrp.R
+++ simulation_final/update_wfpbbmrp.R
@@ -21,18 +21,17 @@ update.wfpbbmrp <- function(){
   for(s in 1:sim){
     
     #-- bootstrap resample
-    bootstrap<-BayesianBootstrap(c(1:n),1)
-    resample<-rmultinom(1,n,bootstrap)
-    bootind <- rep(1:n, resample)
-    
-    boottbl <- 
-      sampled[bootind,] %>%
+  sampled$repwts <- NULL
+  sampled$repwts <- n*normalize(BayesianBootstrap(c(1:n),1) * sampled$wts)
+    boottbl <-
+      sampled%>%
       group_by(J_cell) %>%
-      summarise(bootcts = n(), bootwts = sum(wts))
-    bootsamp_size <- nrow(boottbl)
-    wts_new <- N*normalize(boottbl$bootwts)
-    boot_cats <- boottbl$J_cell
-    
+      summarise(bootwts = sum(repwts))
+    nonzeroind <- boottbl$bootwts != 0
+    boot_cats <- boottbl$J_cell[nonzeroind]
+    wts_new <- N*normalize(boottbl$bootwts[nonzeroind])
+    bootsamp_size <- sum(nonzeroind)
+
     ## find Nj via wfpbb with the bootstrapped categories and new weights
     Nj_est <- rep(0, J)
     
@@ -55,15 +54,13 @@ update.wfpbbmrp <- function(){
     wts_mrp2 <- normalize((Nj_est*cmat2)[sampled_J])
     wts_mrp3 <- normalize((Nj_est*cmat3)[sampled_J])
     wts_mrp4 <- normalize((Nj_est*cmat4)[sampled_J])
-    wts_mrp5 <- normalize((Nj_est*cmat5)[sampled_J])
-    
+
     mrp_posterior[s] <- crossprod(wts_mrp, current_cellmean)
     mrp_posterior1[s] <- crossprod(wts_mrp1, current_cellmean)
     mrp_posterior2[s] <- crossprod(wts_mrp2, current_cellmean)
     mrp_posterior3[s] <- crossprod(wts_mrp3, current_cellmean)
     mrp_posterior4[s] <- crossprod(wts_mrp4, current_cellmean)
-    mrp_posterior5[s] <- crossprod(wts_mrp5, current_cellmean)
-    
+
     
   }
   y_bar_wmrp[i] <<- mean(mrp_posterior,na.rm=T)
@@ -71,7 +68,6 @@ update.wfpbbmrp <- function(){
   y_bar_wmrp2[i] <<- mean(mrp_posterior2,na.rm=T)
   y_bar_wmrp3[i] <<- mean(mrp_posterior3,na.rm=T)
   y_bar_wmrp4[i] <<- mean(mrp_posterior4,na.rm=T)
-  y_bar_wmrp5[i] <<- mean(mrp_posterior5,na.rm=T)
 
 
   ## collect stats from Nj
@@ -100,22 +96,17 @@ update.wfpbbmrp <- function(){
     CI_Njcov_wmrp[sampled_J] <<- CI_Njcov_wmrp[sampled_J] + ((popcts[sampled_J] >= CI_Nj_wmrp[1,]) & (popcts[sampled_J]<=CI_Nj_wmrp[2,]))
   }
   ## collect general stats
-  # CI_Nj_wmrp <<- apply(Njesttemp[,sampled_J], 2, quantile, c(0.025, 0.975), na.rm=T)
-  # CIlength_Nj_wmrp[i,sampled_J] <<- apply(CI_Nj_wmrp,2,diff)
-  # CI_Njcov_wmrp[i,sampled_J] <<- ((popcts[sampled_J] >= CI_Nj_wmrp[1,]) & (popcts[sampled_J] <= CI_Nj_wmrp[2,]))
   CI_wmrp[i,] <<- quantile(mrp_posterior, c(0.025, 0.975),na.rm=T)
   CI_wmrp1[i,] <<- quantile(mrp_posterior1, c(0.025, 0.975),na.rm=T)
   CI_wmrp2[i,] <<- quantile(mrp_posterior2, c(0.025, 0.975),na.rm=T)
   CI_wmrp3[i,] <<- quantile(mrp_posterior3, c(0.025, 0.975),na.rm=T)
   CI_wmrp4[i,] <<- quantile(mrp_posterior4, c(0.025, 0.975),na.rm=T)
-  CI_wmrp5[i,] <<- quantile(mrp_posterior5, c(0.025, 0.975),na.rm=T)
-  
+
   if(is.na(between(true_mean, CI_wmrp[i,1], CI_wmrp[i,2]))){break}
   if(between(true_mean, CI_wmrp[i,1], CI_wmrp[i,2])){CI_covered_wmrp[i] <<- 1}else if(sum(is.na(CI_wmrp[i,])!=0)){CI_covered_wmrp[i] <<- NA}else{CI_covered_wmrp[i] <<- 0}
   if(between(true_mean1, CI_wmrp1[i,1], CI_wmrp1[i,2])){CI_covered_wmrp1[i] <<- 1} else{CI_covered_wmrp1[i] <<- 0}
   if(between(true_mean2, CI_wmrp2[i,1], CI_wmrp2[i,2])){CI_covered_wmrp2[i] <<- 1} else{CI_covered_wmrp2[i] <<- 0}
   if(between(true_mean3, CI_wmrp3[i,1], CI_wmrp3[i,2])){CI_covered_wmrp3[i] <<- 1} else{CI_covered_wmrp3[i] <<- 0}
   if(between(true_mean4, CI_wmrp4[i,1], CI_wmrp4[i,2])){CI_covered_wmrp4[i] <<- 1} else{CI_covered_wmrp4[i] <<- 0}
-  if(between(true_mean5, CI_wmrp5[i,1], CI_wmrp5[i,2])){CI_covered_wmrp5[i] <<- 1} else{CI_covered_wmrp5[i] <<- 0}
-  
+
 }
--- simulation_final/update_wfpbby.R
+++ simulation_final/update_wfpbby.R
@@ -22,9 +22,6 @@ containerfun(basename="bayesvar_wfpbby", subgroups=grps,type="vector",dims=iter)
 
 # WFPBB DIRECT IMPUTATION OF Y -------
 # purpose: update containers with WFPBBY for each iteration in the simulation
-# input: 
-# sample
-# output: 
 
 update.wfpbby <- function(){
   
@@ -34,28 +31,24 @@ for(l in 1:L){
   
   ## (1) Bootstrap from parent sample
   bootstrap<-BayesianBootstrap(c(1:n),1)
-  resample<-rmultinom(1,n,bootstrap)
-  bootind <- rep(1:n, resample)
-  boottbl <- sampled[bootind,] %>%
-    group_by(Y,J_cell) %>%
-    summarise(bootcts = n(), bootwts = sum(wts))
-  
-  bootsamp_size <- nrow(boottbl)
-  wts_new <- N*normalize(boottbl$bootwts)
-  boot_cats <- 1:nrow(boottbl)
+  wts_new <- bootstrap*sampled$wts
+  ind_nonzero <- wts_new!=0
+  wts_new <- wts_new[ind_nonzero]
+  wts_new <- N * normalize(wts_new)
+  boot_cats <- c(1:n)[ind_nonzero]
+  bootsamp_size <- sum(ind_nonzero)
   
   bbmean <- bbmean1 <- bbmean2 <- bbmean3 <- bbmean4 <- bbmean5 <- 0
   ## (2) WFPBB with new weights and unique indices
   fpbb_temp <- rep(0, N)
   for(f in 1:F_draw){
     fpbb_temp <- wtpolyap(ysamp = boot_cats, wts = wts_new, k = N-bootsamp_size)
-    synthpop <- boottbl[fpbb_temp,]
+    synthpop <- sampled[fpbb_temp,]
     bbmean <- bbmean + mean(synthpop$Y)/F_draw
     bbmean1 <- bbmean1 + mean(synthpop$Y[synthpop$J_cell %in% group1])/F_draw
     bbmean2 <- bbmean2 + mean(synthpop$Y[synthpop$J_cell %in% group2])/F_draw
     bbmean3 <- bbmean3 + mean(synthpop$Y[synthpop$J_cell %in% group3])/F_draw
     bbmean4 <- bbmean4 + mean(synthpop$Y[synthpop$J_cell %in% group4])/F_draw
-    bbmean5 <- bbmean5 + mean(synthpop$Y[synthpop$J_cell %in% group5])/F_draw
   }
   
   
@@ -65,7 +58,6 @@ for(l in 1:L){
   tempmean2[l] <- bbmean2
   tempmean3[l] <- bbmean3
   tempmean4[l] <- bbmean4
-  tempmean5[l] <- bbmean5
   #print(l)
 }  
 ## (4) Final estimate for parent sample i
@@ -74,7 +66,6 @@ y_bar_wfpbby1[i] <<- mean(tempmean1,na.rm=T)
 y_bar_wfpbby2[i] <<- mean(tempmean2,na.rm=T)
 y_bar_wfpbby3[i] <<- mean(tempmean3,na.rm=T)
 y_bar_wfpbby4[i] <<- mean(tempmean4,na.rm=T)
-y_bar_wfpbby5[i] <<- mean(tempmean5,na.rm=T)
 
 ## (5) Confidence Interval
 ## (5a) Empirical CI
@@ -83,14 +74,12 @@ CI_wfpbby1[i,] <<- quantile(tempmean1, probs = c(0.025, 0.975),na.rm=T)
 CI_wfpbby2[i,] <<- quantile(tempmean2, probs = c(0.025, 0.975),na.rm=T)
 CI_wfpbby3[i,] <<- quantile(tempmean3, probs = c(0.025, 0.975),na.rm=T)
 CI_wfpbby4[i,] <<- quantile(tempmean4, probs = c(0.025, 0.975),na.rm=T)
-CI_wfpbby5[i,] <<- quantile(tempmean5, probs = c(0.025, 0.975),na.rm=T)  
 
 if(between(true_mean, CI_wfpbby[i,1], CI_wfpbby[i,2])){CI_covered_wfpbby[i] <<- 1}else{CI_covered_wfpbby[i] <<- 0}
 if(between(true_mean1, CI_wfpbby1[i,1], CI_wfpbby1[i,2])){CI_covered_wfpbby1[i] <<- 1}else{CI_covered_wfpbby1[i] <<- 0}
 if(between(true_mean2, CI_wfpbby2[i,1], CI_wfpbby2[i,2])){CI_covered_wfpbby2[i] <<- 1}else{CI_covered_wfpbby2[i] <<- 0}
 if(between(true_mean3, CI_wfpbby3[i,1], CI_wfpbby3[i,2])){CI_covered_wfpbby3[i] <<- 1}else{CI_covered_wfpbby3[i] <<- 0}
 if(between(true_mean4, CI_wfpbby4[i,1], CI_wfpbby4[i,2])){CI_covered_wfpbby4[i] <<- 1}else{CI_covered_wfpbby4[i] <<- 0}
-if(between(true_mean5, CI_wfpbby5[i,1], CI_wfpbby5[i,2])){CI_covered_wfpbby5[i] <<- 1}else{CI_covered_wfpbby5[i] <<- 0}
 
 ## (5b) Estimated CI
 bayesvar_wfpbby[i] <<- var(tempmean)
@@ -98,24 +87,20 @@ bayesvar_wfpbby1[i] <<- var(tempmean1)
 bayesvar_wfpbby2[i] <<- var(tempmean2)
 bayesvar_wfpbby3[i] <<- var(tempmean3)
 bayesvar_wfpbby4[i] <<- var(tempmean4)
-bayesvar_wfpbby5[i] <<- var(tempmean5)
 CI_est_wfpbby[i,] <<- estimatedsefun(L,tempmean)[[2]]
 CI_est_wfpbby1[i,] <<- estimatedsefun(L,tempmean1)[[2]]
 CI_est_wfpbby2[i,] <<- estimatedsefun(L,tempmean2)[[2]]
 CI_est_wfpbby3[i,] <<- estimatedsefun(L,tempmean3)[[2]]
 CI_est_wfpbby4[i,] <<- estimatedsefun(L,tempmean4)[[2]]
-CI_est_wfpbby5[i,] <<- estimatedsefun(L,tempmean5)[[2]]
 SE_est_wfpbby[i] <<- estimatedsefun(L,tempmean)[[1]]
 SE_est_wfpbby1[i] <<- estimatedsefun(L,tempmean1)[[1]]
 SE_est_wfpbby2[i] <<- estimatedsefun(L,tempmean2)[[1]]
 SE_est_wfpbby3[i] <<- estimatedsefun(L,tempmean3)[[1]]
 SE_est_wfpbby4[i] <<- estimatedsefun(L,tempmean4)[[1]]
-SE_est_wfpbby5[i] <<- estimatedsefun(L,tempmean5)[[1]]
 
 if(between(true_mean, CI_est_wfpbby[i,1], CI_est_wfpbby[i,2])){CI_estcovered_wfpbby[i] <<- 1}else{CI_estcovered_wfpbby[i] <<- 0}
 if(between(true_mean1, CI_est_wfpbby1[i,1], CI_est_wfpbby1[i,2])){CI_estcovered_wfpbby1[i] <<- 1}else{CI_estcovered_wfpbby1[i] <<- 0}
 if(between(true_mean2, CI_est_wfpbby2[i,1], CI_est_wfpbby2[i,2])){CI_estcovered_wfpbby2[i] <<- 1}else{CI_estcovered_wfpbby2[i] <<- 0}
 if(between(true_mean3, CI_est_wfpbby3[i,1], CI_est_wfpbby3[i,2])){CI_estcovered_wfpbby3[i] <<- 1}else{CI_estcovered_wfpbby3[i] <<- 0}
 if(between(true_mean4, CI_est_wfpbby4[i,1], CI_est_wfpbby4[i,2])){CI_estcovered_wfpbby4[i] <<- 1}else{CI_estcovered_wfpbby4[i] <<- 0}
-if(between(true_mean5, CI_est_wfpbby5[i,1], CI_est_wfpbby5[i,2])){CI_estcovered_wfpbby5[i] <<- 1}else{CI_estcovered_wfpbby5[i] <<- 0}
 } 
